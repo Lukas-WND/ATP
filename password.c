ea945fa0662978842b3c7d169a955438e3ac12b9
Lukas Wendel <lukas.wrks@gmail.com>
2023-04-25 21:24:02 -0400

    Ex. 1 - Update: use strcmp in strings

--- password.c
+++ password.c
@@ -4,7 +4,7 @@
 
 void main(){
     char password[10], correctPassword[10] = "password", c;
-    int i = 0;
+    int i = 0, result;
 
     printf("Digite a senha \(max 10 digitos\): ");
 
@@ -17,20 +17,11 @@ void main(){
         i++;
     }
 
-    if(password[0] == correctPassword[0] &&
-       password[1] == correctPassword[1] &&
-       password[2] == correctPassword[2] &&
-       password[3] == correctPassword[3] &&
-       password[4] == correctPassword[4] &&
-       password[5] == correctPassword[5] &&
-       password[6] == correctPassword[6] &&
-       password[7] == correctPassword[7] &&
-       password[8] == correctPassword[8] &&
-       password[9] == correctPassword[9]){
-        printf("\nLogin successfuly!\n");
-    }
-    else {
-        printf("\nIncorrect password, try again!\n");
+    result = strcmp(password, correctPassword);
+    if(result == 0){
+        printf("\nLogin correto!");
+    } else {
+        printf("\nSenha incorreta, tente novamente!\n");
         main();
     }
 }
